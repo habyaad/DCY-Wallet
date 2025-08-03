c36bef9711a3dc6f32001728dc105e23ef572eb8
habyaad <adeomotoshan@gmail.com>
2025-08-03 09:36:52 +0100

    feat: create custom gradient brush

---
+++ app/src/main/java/com/example/dcywallet/utils/common/styles/widget_brush.kt
@@ -0,0 +1,8 @@
+package com.example.dcywallet.utils.common.styles
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val gradientBrush = Brush.horizontalGradient(
+    colors = listOf(Color(0xFF8AD4EC), Color(0xFFEF96FF), Color(0xFFFF56A9), Color(0xFFFFAA6C))
+)
