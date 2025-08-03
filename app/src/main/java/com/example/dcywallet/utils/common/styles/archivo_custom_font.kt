151e18b6b89aa1bf1c5b4f0b6347d8ee50d31205
habyaad <adeomotoshan@gmail.com>
2025-08-03 09:35:15 +0100

    feat: create custom font family style

---
+++ app/src/main/java/com/example/dcywallet/utils/common/styles/archivo_custom_font.kt
@@ -0,0 +1,19 @@
+package com.example.dcywallet.utils.common.styles
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.dcywallet.R
+
+val ArchivoFontFamily = FontFamily(
+    Font(R.font.archivo_black, FontWeight.W900),
+    Font(R.font.archivo_extrabold, FontWeight.W800),
+    Font(R.font.archivo_bold, FontWeight.W700),
+    Font(R.font.archivo_semibold, FontWeight.W600),
+    Font(R.font.archivo_medium, FontWeight.W500),
+    Font(R.font.archivo_regular, FontWeight.W400),
+    Font(R.font.archivo_light, FontWeight.W300),
+    Font(R.font.archivo_extralight, FontWeight.W200),
+    Font(R.font.archivo_thin, FontWeight.W100)
+
+)
