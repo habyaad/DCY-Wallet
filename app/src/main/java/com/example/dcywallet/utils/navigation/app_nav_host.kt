68f07a6aae03ec5d602c5bd37889f94cc2ac555f
habyaad <adeomotoshan@gmail.com>
2025-08-08 21:44:21 +0100

    feat: add import wallet route to app nav host

--- app/src/main/java/com/example/dcywallet/utils/navigation/app_nav_host.kt
+++ app/src/main/java/com/example/dcywallet/utils/navigation/app_nav_host.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.dcywallet.auth.ImportWalletScreen
 import com.example.dcywallet.onboarding.OnBoardingScreen
 import com.example.dcywallet.onboarding.WalletSetupScreen
 
@@ -12,6 +13,6 @@ fun AppNavHost(navController: NavHostController){
     NavHost(navController = navController, startDestination = Screens.Onboarding.route){
         composable(Screens.Onboarding.route){ OnBoardingScreen(navController) }
         composable(Screens.WalletSetup.route){ WalletSetupScreen(navController) }
-        composable(Screens.ImportWallet.route){ OnBoardingScreen(navController) }
+        composable(Screens.ImportWallet.route){ ImportWalletScreen(navController) }
     }
 }
